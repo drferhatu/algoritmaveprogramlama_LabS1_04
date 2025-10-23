7a647fbbac136315fc1ae22bafd2dd1cf1d60f15
Ferhat Ucar <67374172+drferhatu@users.noreply.github.com>
2025-10-23 22:33:19 +0300

    Implement student information system in OgrenciBilgi.java
    Added a  template Java program to collect and display student information.

--- OgrenciBilgi.java
+++ OgrenciBilgi.java
@@ -1,1 +1,59 @@
+*
+ * Ad Soyad: [ADINIZI BURAYA YAZIN]
+ * Ogrenci No: [OGRENCI NUMARANIZI BURAYA YAZIN]
+ * Tarih: [TARIHI BURAYA YAZIN]
+ * Aciklama: Gorev 1 - Ogrenci Bilgi Sistemi
+ * 
+ * Bu program kullanicidan ogrenci bilgilerini alir ve
+ * duzenli bir formatta ekrana yazdirir.
+ */
 
+import java.util.Scanner;
+
+public class OgrenciBilgi {
+    public static void main(String[] args) {
+        // Scanner objesi olusturun
+        
+        
+        // Degisken tanimlamalari
+        // String ad, soyad;
+        // int ogrenciNo, yas;
+        // double gpa;
+        
+        
+        // Kullanicidan bilgileri alin
+        System.out.println("=== OGRENCI BILGI SISTEMI ===");
+        System.out.println();
+        
+        // Ad
+        
+        
+        // Soyad
+        
+        
+        // Ogrenci No
+        
+        
+        // Yas
+        
+        
+        // GPA
+        
+        
+        // Bilgileri ekrana yazdirin
+        System.out.println("\n=== OGRENCI BILGI SISTEMI ===");
+        
+        // COZUMUNUZU BURAYA YAZIN
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        // Scanner'i kapatin (önemli pratik)
+        
+    }
+}
